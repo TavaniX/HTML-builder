69051635247806d15fa5ecf3e1e5f03870fb0ed4
Maksim Aisin <maksim.aisin@mail.ru>
2022-05-17 23:51:48 +0300

    feat: files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const {readdir, lstat} = require('fs');
+const folderPath = path.join(__dirname, 'secret-folder');
+
+readdir(folderPath, {withFileTypes: true}, (err, files)=>{
+  if(err) {
+    console.log(err);
+  } else {
+    files.forEach(file => {
+      if(file.isFile()) {
+        let fileFullPath = path.join(folderPath, file.name);
+        let fileProps = path.parse(fileFullPath);
+        lstat(fileFullPath, (err, stats) => {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log(fileProps.name + ' - ' + fileProps.ext.slice(1, 99) + ' - ' + stats.size + 'kb' );
+          }
+        });
+        
+      }
+    });
+  }
+});
