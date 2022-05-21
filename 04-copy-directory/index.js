4a3aa6c23ebe8a90b00333f58baae33d8711c426
Maksim Aisin <maksim.aisin@mail.ru>
2022-05-21 20:48:56 +0300

    fix: files-in-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const fsPromises = require('fs').promises;
+const currentFolderPath = path.join(__dirname, 'files');
+const newFolderPath = path.join(__dirname, 'files-copy');
 
+const copyDir = async (newFolder, currentFolder) => {
+  await fsPromises.rm(newFolder, {
+    recursive: true,
+    force: true,
+  });
+
+  await fsPromises.mkdir(newFolder, {recursive: true});
+
+  const files = await fsPromises.readdir(currentFolder, {withFileTypes: true});
+
+  files.forEach(async file => {
+    if(file.isFile()) {
+      let currentFile = path.join(currentFolder, file.name);
+      let newFile = path.join(newFolder, file.name);
+      await fsPromises.copyFile(currentFile, newFile);
+    }
+  });
+};
+
+copyDir(newFolderPath, currentFolderPath);
